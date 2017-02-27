4ba24320bf058a456cfb7c7393b812cb6c8b8e20
zhwei <zhwei.yes@gmail.com>
2017-02-27 10:46:03 +0800

    页面改版

--- resources/views/demo.blade.php
+++ resources/views/demo.blade.php
@@ -1,13 +1,15 @@
 <!doctype html>
 <html lang="en">
 <head>
-    <title>{{ $title }}</title>
+    <title>{{ $title }} &middot; Laravel Lego Demo</title>
     @include('lego::styles')
     <link href="//cdn.bootcss.com/highlight.js/9.9.0/styles/github.min.css" rel="stylesheet">
     <style>
         #body {
             margin: 0 auto;
             float: none;
+            min-height: 2000px;
+            padding-top: 70px;
         }
 
         pre code {
@@ -17,19 +19,48 @@
 </head>
 <body>
 
+<!-- Fixed navbar -->
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Lego Demo</a>
+        </div>
+        <div id="navbar" class="navbar-collapse collapse">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Demos <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        @foreach($demos as $item => $name)
+                            <li><a href="{{ action('LegoController@demo', $item) }}">{{ $name }}</a></li>
+                        @endforeach
+                    </ul>
+                </li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="https://github.com/wutongwan/laravel-lego">GitHub</a></li>
+                <li><a href="https://github.com/zhwei/lego-demo">Demo Repo</a></li>
+                <li><a href="https://github.com/zhwei">Author：@zhwei</a></li>
+            </ul>
+        </div><!--/.nav-collapse -->
+    </div>
+</nav>
+
 <div id="body" class="col-md-12">
-    <h1 class="text-center">{{ $title }} &middot; Lego Demo</h1>
-    <hr>
     <ul>
         @foreach($demos as $item => $name)
             <li><a href="{{ action('LegoController@demo', $item) }}">{{ $name }}</a></li>
         @endforeach
     </ul>
     <hr>
-
     <div class="col-md-6">
         <div class="panel panel-default">
-            <div class="panel-heading text-center">Code</div>
+            <div class="panel-heading text-center">{{ $title }} &middot; Code</div>
             <div class="panel-body">
                 <pre><code class="php">{!! $code !!}</code></pre>
             </div>
@@ -37,7 +68,7 @@
     </div>
     <div class="col-md-6">
         <div class="panel panel-default">
-            <div class="panel-heading text-center">Demo</div>
+            <div class="panel-heading text-center">{{ $title }} &middot; Demo</div>
             <div class="panel-body">
                 {!! $widget !!}
             </div>
