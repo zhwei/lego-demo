56a1afb0c88c5632af293730f33dec6420c67d95
zhwei <zhwei.yes@gmail.com>
2017-02-27 11:39:36 +0800

    fix title wrong

--- app/Http/Controllers/LegoController.php
+++ app/Http/Controllers/LegoController.php
@@ -7,11 +7,11 @@ class LegoController extends Controller
     private function demos()
     {
         return [
-            'city-list' => 'Form：城市列表',
+            'city-list' => 'Filter & Grid：城市列表',
             'city' => 'Form：新建/编辑城市',
-            'street-list' => 'Form：街道列表',
+            'street-list' => 'Filter & Grid：街道列表',
             'street' => 'Form：新建/编辑街道',
-            'suite-list' => 'Form：公寓列表',
+            'suite-list' => 'Filter & Grid：公寓列表',
             'suite' => 'Form：新建/编辑公寓',
         ];
     }
