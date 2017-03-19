637a60a60d5324a159edcc3d4e02807defd9e61f
zhwei <zhwei.yes@gmail.com>
2017-03-19 09:36:17 +0800

    add grid export example

--- config/app.php
+++ config/app.php
@@ -177,6 +177,7 @@ return [
         App\Providers\RouteServiceProvider::class,
 
         Collective\Html\HtmlServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
         Lego\LegoServiceProvider::class,
     ],
 
